c81ada8f49d27fa640440927a7ad8daf3a07b39b
jhtan <jhtan@jhtan.com>
2014-06-10 10:28:24 -0400

    values for a, b and r

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready( function () {
+    var X = [], Y = []; // the data of the graph.
+
     /*var sinPoints = []; 
     for (var i=0; i<2*Math.PI; i+=0.1){ 
 	sinPoints.push([i, 2*Math.sin(i-.8)]); 
@@ -13,18 +15,45 @@ $(document).ready( function () {
     });
 
     function addPair(x, y) {
-	$("#dataTable").prepend("<tr><td>"+x+"</td><td>"+y+"</td></tr>");
-	
-	var data = [[]];
-	data[0].push([x, y]);
-	return data;
+        $("#dataTable").prepend("<tr><td>"+x+"</td><td>"+y+"</td></tr>");
+
+        X.push(x);
+        Y.push(y);
+        calculateA();
+        calculateB();
+        calculateX();
+
+        var data = [[]];
+        data[0].push([x, y]);
+        return data;
+    }
+
+    /**
+     * Calculate the a value.
+     */
+    function calculateA() {
+        var a = 0;
+        for(var i = 0; i< X.length; i++) {
+            a += parseInt(X[i]);
+        }
+
+        $('#aValue').text(a);
+    }
+
+    function calculateB() {
+
+    }
+
+    function calculateX() {
+
     }
 
     $("#newXCoordinate, #newYCoordinate").change(function () {
-	if($("#newXCoordinate").val() && $("#newYCoordinate").val()) {
-	    addPair($("#newXCoordinate").val(), $("#newYCoordinate").val());
-	    $("#newXCoordinate").val("");
-	    $("#newYCoordinate").val("");
-	}
+        if($("#newXCoordinate").val() && $("#newYCoordinate").val()) { // validate null
+            addPair($("#newXCoordinate").val(), $("#newYCoordinate").val()); // call to addPair
+
+            $("#newXCoordinate").val(""); // clear the textbox
+            $("#newYCoordinate").val(""); // clear the textbox
+        }
     });
 });
