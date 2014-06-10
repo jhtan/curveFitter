eb09e339ac14d8a0235f34884c7bc9967eed9a94
anficyon <anficyon@gmail.com>
2014-06-10 12:34:53 -0400

    funciones a,b,r

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready( function () {
-    var X = [], Y = []; // the data of the graph.
-
+    var X = [2,4,6,8,10,12,15], Y = [0,10,18,32,45,52,65]; // the data of the graph.
+    calculateA();
+    calculateB();
+    calculateR();
     /*var sinPoints = []; 
     for (var i=0; i<2*Math.PI; i+=0.1){ 
 	sinPoints.push([i, 2*Math.sin(i-.8)]); 
@@ -28,23 +30,117 @@ $(document).ready( function () {
         return data;
     }
 
+
     /**
-     * Calculate the a value.
-     */
+     
+*/
+    function Sumx(){
+        var sumx=0;
+        
+        for(var i=0; i< X.length; i++)
+        {
+        sumx+=parseInt(X[i]);
+        }
+        return parseInt(sumx);
+    }
+    function Sumy(){
+        var sumy=0;
+        
+        for(var i=0; i< Y.length; i++)
+        {
+        sumy+=parseInt(Y[i]);
+        }
+        return parseInt(sumy);
+    }
+    
+    function Sumx2()
+    {
+        var sumx2=0;
+        var x2=0;
+        for(var i=0;i< X.length; i++)
+        {
+            sumx2 += (parseInt(X[i])*parseInt(X[i]));
+            //x2=(parseInt(X[i])*parseInt(X[i]));
+            //sumx2+=parseInt(x2);
+            //alert("sumx2 = " + sumx2);
+        }
+
+        //alert("sumx2 = " + sumx2);
+
+        return parseInt(sumx2);
+    }
+    function Sumy2()
+    {
+        var sumy2=0;
+        var y2=0;
+        for(var i=0;i< Y.length; i++)
+        {
+            y2=(parseInt(Y[i])*parseInt(Y[i]));
+            sumy2+=y2;
+        }
+        return parseInt(sumy2);
+    }
+    function Sumxy()
+    {
+        var sumxy=0;
+        var xy=0;
+        for(var i=0;i< X.length; i++)
+        {
+            xy=(parseInt(X[i])*parseInt(Y[i]));
+            sumxy+=xy;
+        }
+        return parseInt(sumxy);
+    }
+    /**
+    
+    
+    */
+
+
     function calculateA() {
         var a = 0;
-        for(var i = 0; i< X.length; i++) {
+        /**for(var i = 0; i< X.length; i++) {
             a += parseInt(X[i]);
         }
+        */
+
+        var n=0;
+        n=(Sumx2()*Sumy())-(Sumx()*Sumxy());
+        var  m=0;
+        m=(X.length*Sumx2())-((Sumx())*(Sumx()));
+        //a=Sumx();
+        //alert(n);
+        //alert(m);
+        a=parseInt(n)/parseInt(m);
+        //a=Sumx2();
 
         $('#aValue').text(a);
     }
 
     function calculateB() {
-
+        var b=0;
+        var n=0;
+        n=(X.length*Sumxy())-(Sumx()*Sumy()); 
+        var m=0;
+        m=(X.length*Sumx2())-(Sumx()*Sumx());
+        
+        b=parseInt(n)/parseInt(m);
+        $('#bValue').text(b);
     }
 
-    function calculateX() {
+    function calculateR() {
+        var r=0;
+        var n=0;
+        n = (X.length*Sumxy()) - (Sumx()*Sumy());
+        var m=0;
+        var raiz=1;
+        raiz = (X.length*Sumx2()-(Sumx()*Sumx()))*(X.length*Sumy2()-(Sumy()*Sumy())); 
+        m = Math.sqrt(raiz);
+        r=parseInt(n)/parseInt(m);
+
+        $('#rValue').text(r);
+
+
 
     }
 
